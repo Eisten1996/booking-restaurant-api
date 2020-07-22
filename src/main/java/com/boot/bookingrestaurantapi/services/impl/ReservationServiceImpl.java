4b96efb01936837b8b2d58d02ae4a7fd8a6ffa1f
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-07-22 14:54:24 -0500

    Crear el Servicio de Realizar Reserva - Guardar nuestros datos de Reserva

--- src/main/java/com/boot/bookingrestaurantapi/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/boot/bookingrestaurantapi/services/impl/ReservationServiceImpl.java
@@ -4,12 +4,15 @@ package com.boot.bookingrestaurantapi.services.impl;
 import java.util.stream.Collectors;*/
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.boot.bookingrestaurantapi.entities.Reservation;
 import com.boot.bookingrestaurantapi.entities.Restaurant;
 import com.boot.bookingrestaurantapi.entities.Turn;
 import com.boot.bookingrestaurantapi.execptions.BookingException;
+import com.boot.bookingrestaurantapi.execptions.InternalServerErrorException;
 import com.boot.bookingrestaurantapi.execptions.NotFountException;
 import com.boot.bookingrestaurantapi.jsons.CreateReservationRest;
 import com.boot.bookingrestaurantapi.jsons.ReservationRest;
@@ -20,9 +23,15 @@ import com.boot.bookingrestaurantapi.services.ReservationService;
 
 public class ReservationServiceImpl implements ReservationService {
 
+	private static final Logger LOOGER = LoggerFactory.getLogger(ReservationServiceImpl.class);
+
 	@Autowired
-	ReservationRepository reservationRepository;
+	private ReservationRepository reservationRepository;
+
+	@Autowired
 	private RestaurantRepository restaurantRepository;
+
+	@Autowired
 	private TurnRepository turnRepository;
 
 	public static final ModelMapper modelMapper = new ModelMapper();
@@ -41,7 +50,7 @@ public class ReservationServiceImpl implements ReservationService {
 	public String createReservation(CreateReservationRest createReservationRest) throws BookingException {
 		final Restaurant restaurantId = restaurantRepository.findById(createReservationRest.getRestaurantId())
 				.orElseThrow(() -> new NotFountException("RESTAURANT_NOT_FOUND", "RESTAURANT_NOT_FOUND"));
-		final Turn turnId = turnRepository.findById(createReservationRest.getTurnId())
+		final Turn turn = turnRepository.findById(createReservationRest.getTurnId())
 				.orElseThrow(() -> new NotFountException("TURN_NOT_FOUND", "TURN_NOT_FOUND"));
 		String locator = generateLocator(restaurantId, createReservationRest);
 		final Reservation reservation = new Reservation();
@@ -49,8 +58,15 @@ public class ReservationServiceImpl implements ReservationService {
 		reservation.setPerson(createReservationRest.getPerson());
 		reservation.setDate(createReservationRest.getDate());
 		reservation.setRestaurant(restaurantId);
-		reservation.setTurn(turnId);
-		return null;
+		reservation.setTurn(turn.getName());
+
+		try {
+			reservationRepository.save(reservation);
+		} catch (final Exception e) {
+			LOOGER.error("INTERNAL_SERVER_ERROR");
+			throw new InternalServerErrorException("INTERNAl_SERVER_ERROR", "INTERNAl_SERVER_ERROR");
+		}
+		return locator;
 	}
 
 	private Reservation getReservationEntity(Long reservationId) throws BookingException {
